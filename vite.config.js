afe1aa9390f9c0c2b7794794c0731a8b8e3641a7
Nicholas VanFossen <35882058+vanfossen@users.noreply.github.com>
2024-10-07 20:35:57 -0400

    update base name

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/simple-qrcode-generator/",
+  base: "/simple-landing-page/",
   plugins: [react()],
-})
+});
